a5214758f703d9eb8fb6d96f48272863464484bd
MollytheCatLoca <101350226+MollytheCatLoca@users.noreply.github.com>
2024-07-10 19:07:15 -0300

    Initial commit for portfolio project

---
+++ components/RecentProjects.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { FaLocationArrow } from "react-icons/fa6";
+import { projects } from "@/data";
+import { PinContainer } from "./ui/PinContainer";
+
+const RecentProjects = () => {
+  return (
+    <div className="py-20" id="projects">
+      <h1 className="heading-responsive mb-10">
+        Proyectos que {" "}
+        <span className="text-purple">Potencian</span>
+      </h1>
+      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+        {projects.map((item) => (
+          <PinContainer
+            key={item.id}
+            title={item.title}
+            href={item.link}
+            containerClassName="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+          >
+            <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+              <div
+                className="relative w-full h-full overflow-hidden lg:rounded-3xl"
+                style={{ backgroundColor: "#13162D" }}
+              >
+                <img src="/bg.png" alt="bgimg" className="absolute inset-0 object-cover w-full h-full" />
+              </div>
+              <img
+                src={item.img}
+                alt="cover"
+                className="z-10 absolute bottom-0 h-full w-full rounded-lg object-cover"
+              />
+            </div>
+
+            <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+              {item.title}
+            </h1>
+
+            <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2" style={{ color: "#BEC1DD", margin: "1vh 0" }}>
+              {item.des}
+            </p>
+
+            <div className="flex items-center justify-between mt-7 mb-3">
+              <div className="flex items-center">
+                {item.iconLists.map((icon, index) => (
+                  <div
+                    key={index}
+                    className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                    style={{ transform: `translateX(-${4 * index + 3}px)` }}
+                  >
+                    <img src={icon} alt="icon5" className="object-cover w-full h-full rounded-full transform scale-125" />
+                  </div>
+                ))}
+              </div>
+
+              <div className="flex justify-center items-center">
+                <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                  + Info
+                </p>
+                <FaLocationArrow className="ms-3" color="#CBACF9" />
+              </div>
+            </div>
+          </PinContainer>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecentProjects;
---
+++ data/index.ts
@@ -0,0 +1,233 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Exitos", link: "#exitos" },
+    { name: "Contacto", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "Liderazgo en Soluciones Energéticas Sostenibles",
+      description: "Empoderamos municipios, empresas y comunidades con soluciones renovables innovadoras.",
+      className:"lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh] bg-gradient-to-r from-blue-500 via-blue-300 to-blue-100",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end bg-opacity-70",
+      img: "/SolarTech1.jpg",
+      spareImg: "",
+    },
+    {
+        id: 2,
+        title: "Consultoría Integral en Energías Renovables",
+        description: "Desde estudios de viabilidad hasta la optimización de sistemas energéticos.",
+        className: "relative lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "absolute top-0 left-0 z-10 p-4 text-white",
+    },
+    {
+      id: 3,
+      title: "Soporte Financiero para Proyectos Energéticos",
+      description: "Gestionamos líneas financieras nacionales e internacionales para viabilizar tus proyectos.",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Parques Solares All-in-One",
+      description: "Soluciones llave en mano que simplifican la instalación y operación de sistemas de energía solar.",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "h-full w-full object-cover object-center",
+      titleClassName: "justify-start",
+      img: "/BatsPanel.jpg",
+      spareImg: "",
+    },
+    {
+        id: 5,
+        title: "Transformando Comunidades con Energía Limpia",
+        description: "Proyectos que convierten a las localidades en ejemplos de sostenibilidad y eficiencia.",
+        className: "relative md:col-span-3 md:row-span-2",
+        imgClassName: "absolute right-0 bottom-0 h-full w-full object-cover object-center",
+        titleClassName: "justify-end bg-black bg-opacity-50 text-white",
+        img: "/SolarTech6.jpg",
+        spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Únete a Nuestra Misión Sostenible",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+
+
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Soluciones All-in-One",
+      des: "Parques solares con toda la electrónica necesaria en un container preconfigurado, listos para su uso.",
+      img: "/BatsPanel.jpg",
+      iconLists: ["/solar.svg", "/efficiency.svg", "/innovation.svg"],
+      link: "https://bisintegraciones.com/transformando-municipios-en-comunidades-sustentables-con-energias-renovables/",
+    },
+    {
+      id: 2,
+      title: "Energía Verde para Edificios Públicos",
+      des: "Instalación de paneles solares en colegios y hospitales para reducir costos y mejorar la sostenibilidad.",
+      img: "/edificiosSolar.jpg",
+      iconLists: ["/public_buldings.svg", "/solar.svg", "/sustainability.svg"],
+      link: "https://bisintegraciones.com/soluciones-integrales-en-energias-renovables/",
+    },
+    {
+      id: 3,
+      title: "Parques Solares",
+      des: "Generación de energía verde a pequeña escala para reducir costos energéticos y fomentar la sostenibilidad.",
+      img: "/SolarTech5.jpg",
+      iconLists: ["/solar.svg", "/sustainability.svg", "/efficiency.svg"],
+      link: "https://bisintegraciones.com/soluciones-integrales-en-energias-renovables/",
+    },
+    {
+      id: 4,
+      title: "Consultoría Energética Integral",
+      des: "Asesoría completa desde estudios de viabilidad hasta la implementación y optimización de sistemas.",
+      img: "/consultoriaener.jpg",
+      iconLists: ["/consulting.svg", "/efficiency.svg", "/sustainability.svg"],
+      link: "https://bisintegraciones.com/soluciones-integrales-en-energias-renovables/",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+        quote: "En BIS Integraciones, hemos implementado parques solares All-in-One que transforman comunidades con su eficiencia. Por ejemplo, nuestro parque solar de 900 kW genera 1384 MWh anuales, proporcionando ahorros significativos y un retorno de inversión del 16% con repago en 5 años.",
+        name: "Innovación en Acción",
+        title: "Parque Solar All-in-One"
+    },
+    {
+        quote: "Ofrecemos soluciones tecnológicas personalizadas y eficiente gestión de proyectos. Nuestra integración de IT, AI y energías renovables ha revolucionado infraestructuras, optimizando procesos e incrementando la eficiencia operativa de nuestros clientes.",
+        name: "Soluciones Integradas",
+        title: "Consultoría Integral"
+    },
+    {
+        quote: "Nuestros parques solares, como el de 900 kW, generan hasta 1384 MWh anualmente, logrando ahorros y eficiencia sin precedentes. Asesoramos a empresas en el uso de tecnología avanzada para maximizar el rendimiento económico y operativo.",
+        name: "Liderando el Futuro",
+        title: "Innovación en Energías Renovables"
+    },
+    {
+        quote: "En BIS Integraciones, desarrollamos software a medida y soluciones AI para automatizar flujos de trabajo y apoyar decisiones estratégicas basadas en datos. Nuestra finalidad es garantizar que nuestros clientes estén siempre a la vanguardia de la sostenibilidad e innovación.",
+        name: "Eficiencia y Sostenibilidad",
+        title: "Integración e Innovación"
+    },
+    {
+        quote: "A través de nuestro acuerdo con BID Invest, facilitamos la financiación internacional para la implementación de parques solares de alta eficiencia. Estos parques reducen costos energéticos mientras incrementan la sostenibilidad de nuestros clientes.",
+        name: "Financiación Global",
+        title: "Consultoría Financiera"
+    },
+    {
+        quote: "Nuestra consultoría financiera ha simplificado el acceso a financiamiento del BICE, permitiendo la inversión en parques solares que generan hasta 351,157 kWh mensuales. En BIS Integraciones, nuestras soluciones innovadoras posicionan a nuestros clientes como líderes en eficiencia energética.",
+        name: "Resultados Tangibles",
+        title: "Consultoría y Financiación"
+    }
+];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "Huawei",
+      img: "",
+      nameImg: "/huawei_logo_PNG1.png",
+    },
+    {
+      id: 2,
+      name: "greenpower",
+      img: "",
+      nameImg: "Abb_logo_PNG6.png",
+    },
+    {
+      id: 3,
+      name: "renewableenergy",
+      img: "",
+      nameImg: "/Trina.svg",
+    },
+    {
+      id: 4,
+      name: "Honeywell",
+      img: "",
+      nameImg: "/Honeywell-Logo-1.svg",
+    },
+    {
+      id: 5,
+      name: "Siri",
+      img: "",
+      nameImg: "/Siri-Logo-SVG_010.svg",
+    },
+
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Ingeniería en Energías Renovables",
+      desc: "Contamos con un equipo de ingenieros especializados en el diseño y desarrollo de proyectos de energías renovables. Nuestra experiencia incluye la implementación de tecnologías avanzadas para maximizar la eficiencia y sostenibilidad de los sistemas energéticos.",
+      className: "md:col-span-2",
+      thumbnail: "/ingenieria1.webp",
+    },
+    {
+      id: 2,
+      title: "Consultoría Financiera y Project Finance",
+      desc: "Ofrecemos asesoramiento integral en la estructuración y gestión de financiamiento para proyectos de energía renovable. Nuestra experiencia abarca desde la obtención de financiamiento con bancos nacionales e internacionales hasta la gestión de fondos de organismos multilaterales.",
+      className: "md:col-span-2",
+      thumbnail: "/finance.webp",
+    },
+    {
+      id: 3,
+      title: "Gestión de Proyectos y Procurement",
+      desc: "Proveemos soluciones completas de gestión de proyectos, desde la planificación y adquisición de recursos hasta la implementación y monitoreo. Nuestro equipo asegura la calidad y eficiencia en cada etapa del proyecto.",
+      className: "md:col-span-2",
+      thumbnail: "/procurement.webp",
+    },
+    {
+      id: 4,
+      title: "Implementación de Parques Solares All-in-One",
+      desc: "Diseñamos e implementamos parques solares con soluciones 'All-in-One' que incluyen toda la electrónica necesaria en un container preconfigurado. Esta solución facilita una instalación rápida y un mantenimiento simplificado.",
+      className: "md:col-span-2",
+      thumbnail: "/allinone.webp",
+    },
+    {
+      id: 5,
+      title: "Consultoría Energética Integral",
+      desc: "Brindamos asesoría completa en estudios de viabilidad, optimización de sistemas energéticos y sostenibilidad. Nuestro enfoque estratégico asegura que los proyectos no solo sean eficientes sino también rentables a largo plazo.",
+      className: "md:col-span-2",
+      thumbnail: "/consuener.webp",
+    },
+    {
+      id: 6,
+      title: "Innovación y Tecnología",
+      desc: "Nos mantenemos a la vanguardia de la tecnología, integrando soluciones innovadoras en todos nuestros proyectos. Utilizamos las últimas herramientas y tecnologías para asegurar que nuestros clientes obtengan los mejores resultados posibles.",
+      className: "md:col-span-2",
+      thumbnail: "/innovacion.webp",
+    },
+  ];
+  
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/link.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/git.svg",
+    },
+  ];
+  
