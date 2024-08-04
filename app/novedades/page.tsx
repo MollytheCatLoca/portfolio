764ab28a526d055d09cc9fa5eac27dcf8a6c524e
MollytheCatLoca <101350226+MollytheCatLoca@users.noreply.github.com>
2024-08-04 20:07:49 -0300

    path novedades

---
+++ app/novedades/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+// app/old-page/page.tsx
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const OldPageRedirect = () => {
+    const router = useRouter();
+    const newPath = '/'; // La nueva ruta a la que quieres redirigir
+
+    useEffect(() => {
+        router.replace(newPath);
+    }, [router, newPath]);
+
+    return null; // No renderiza ningún contenido
+};
+
+export default OldPageRedirect;
