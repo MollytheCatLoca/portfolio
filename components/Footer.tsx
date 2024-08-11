8db0e6337c1173d9bd4be3d6f9bc7e974d76e25a
MollytheCatLoca <101350226+MollytheCatLoca@users.noreply.github.com>
2024-08-11 19:34:58 -0300

    footerHero

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,33 +16,28 @@ const Footer = () => {
     Innovación y tecnología para un futuro más eficiente y verde.
         </p>
 
-      < a href = "mailto:contacto@bisintegraciones.com" >
-        <MagicButton
-            title="Contáctanos"
-icon = {< FaLocationArrow />}
-position = "right"
-  />
-  </a>
-  </div>
-  < div className = "flex mt-16 md:flex-row flex-col justify-between items-center" >
-    <p className="md:text-base text-sm md:font-normal font-light" >
-      Copyright © 2024 BIS Integraciones
-        </p>
 
-        < div className = "flex items-center md:gap-3 gap-6 mt-4 md:mt-0" >
-        {
-          socialMedia.map((info) => (
-            <div
+      </a>
+      </div>
+      < div className = "flex mt-16 md:flex-row flex-col justify-between items-center" >
+        <p className="md:text-base text-sm md:font-normal font-light" >
+          Copyright © 2024 BIS Integraciones
+            </p>
+
+            < div className = "flex items-center md:gap-3 gap-6 mt-4 md:mt-0" >
+            {
+              socialMedia.map((info) => (
+                <div
               key= { info.id }
               className = "w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-            <img src={ info.img } alt = "icons" width = { 20} height = { 20} />
-            </div>
-          ))
-        }
-          </div>
-          </div>
-          </footer>
+                >
+                <img src={ info.img } alt = "icons" width = { 20} height = { 20} />
+                </div>
+              ))
+            }
+              </div>
+              </div>
+              </footer>
   );
 };
 
