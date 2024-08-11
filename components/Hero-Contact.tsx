54776142c1f4bad851d74002779c2a720e216eb6
MollytheCatLoca <101350226+MollytheCatLoca@users.noreply.github.com>
2024-08-11 19:24:54 -0300

    contactUS3

--- components/Hero-Contact.tsx
+++ components/Hero-Contact.tsx
@@ -62,8 +62,59 @@ const Hero = () => {
             < MagicButton title = "Home" icon = {< FaLocationArrow />} position = "right" />
                 </a>
                 </div>
-                </div>
-                </div>
+                < /div>import { FaLocationArrow } from "react-icons/fa6";
+import MagicButton from "./ui/MagicButton";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/Text-Generate-Effect";
+
+const Hero = () => {
+    return (
+        <div className= "pb-1 pt-40" >
+        {/* UI: Spotlights */ }
+        < div >
+        <Spotlight
+                          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+    fill = "white"
+        />
+        <Spotlight
+                          className="h-[80vh] w-[50vw] top-10 left-full"
+    fill = "purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill = "blue" />
+            </div>
+
+    {/* UI: grid */ }
+    <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2] absolute top-0 left-0 flex items-center justify-center" >
+        {/* Radial gradient for the container to give a faded look */ }
+        < div className = "absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+            </div>
+
+            < div className = "flex justify-center relative my-20 z-10" >
+                <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center" >
+                    <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80" >
+                        BIS Integraciones
+                            </p>
+
+                            < TextGenerateEffect
+    words = "Liderando la Transformación Sostenible con Innovación y Tecnología"
+    className = "text-center text-[40px] md:text-5xl lg:text-6xl"
+        />
+
+        <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl text-white" >
+            En BIS Integraciones, lideramos la transformación hacia un futuro sostenible, integrando tecnología avanzada en energías renovables, IT y soluciones viales.
+                          </p>
+
+                < a href = "/" >
+                    <MagicButton title="Home" icon = {< FaLocationArrow />} position = "right" />
+                        </a>
+                        </div>
+                        </div>
+                        </div>
+                  );
+                };
+
+export default Hero;
+</div>
   );
 };
 
