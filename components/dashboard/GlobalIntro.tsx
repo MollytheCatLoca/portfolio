11bc225dcd816e241dc9529548d159ac3f61f9b7
MollytheCatLoca <101350226+MollytheCatLoca@users.noreply.github.com>
2024-09-13 17:33:33 -0300

    modi en card3

--- components/dashboard/GlobalIntro.tsx
+++ components/dashboard/GlobalIntro.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/Card3';
 import { Sun, Zap, LineChart, Package, Clock, DollarSign } from 'lucide-react';
 
 const ExecutiveIntro = ({ simulationData }) => {
